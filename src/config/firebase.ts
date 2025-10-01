d651b6c15170e87a6dc1a7e02f7164b4c6253032
HarshaJyos <harshajyosyabhatla@gmail.com>
2025-10-01 11:49:43 +0530

    third commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getMessaging } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const messaging = getMessaging(app);
 
-export { app, auth, messaging, firebaseConfig };
+export { app, auth, firebaseConfig };
